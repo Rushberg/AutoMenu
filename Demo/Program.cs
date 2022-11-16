eed85988372279917a1c2fee55c1bb4f207b6fa7
Alexey Yarygin <alexey.yarygin@intel.com>
2022-11-16 15:22:15 +0200

    Added demo

---
+++ Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AutoMenu;
+
+namespace AutoMenu;
+
+public static class App
+{
+    public static void Main(string[] args) {
+        var mainMenu = new SubMenu("Main", true);
+        var subMenu = new SubMenu("Second Level");
+        mainMenu.AddOption("Go To Second", typeof(SubMenu).GetMethod(nameof(subMenu.Show)), subMenu);
+        mainMenu.AddOption("Say Hello", typeof(App).GetMethod(nameof(SayHello)), null);
+        
+        subMenu.AddOption(null, typeof(App).GetMethod(nameof(LotsOfArgs)), null);
+        
+        mainMenu.Show();
+    }
+
+    public static string SayHello(string name) {
+        return $"Hello {name}";
+    }
+
+    public static string LotsOfArgs(bool they, string are, int indeed, double validated) {
+        return $"They {they} are {are} indeed {indeed} validated {validated}";
+    }
+}
